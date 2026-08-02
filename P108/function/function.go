9fdb64caff538ad1fe5434ff98870acbf2979dfa
chenjiajun <1437291129@qq.com>
2026-08-02 16:11:49 +0800

    完善并结束P108任务，增加函数底层以及return细节

--- P108/function/function.go
+++ P108/function/function.go
@@ -20,3 +20,20 @@ func Calculate(a float64, b float64, operate byte) float64 { // 这里最后指�
 	}
 	return result
 }
+
+func Test(n1 int) {
+	n1 += 1
+	fmt.Printf("Test函数中的 n1 = %d ", n1)
+}
+
+// Sum 返回值类型列表
+func Sum(n1 int, n2 int) int {
+	return n1 + n2
+} // 有返回值的函数，会把计算出的最终结果交给调用者
+
+// SumSub 双返回值类型演示
+func SumSub(a int, b int) (int, int) { // 可以看到这里写的是双返回值类型
+	sum := a + b
+	sub := a - b
+	return sum, sub
+} // go 返回多个值不用什么数组啥的，直接标注再返回就行
--- P108/function/main/P108.go
+++ P108/function/main/P108.go
@@ -15,4 +15,18 @@ func main() {
 	prediction := [4]float64{120.0, 120.0, 220.0, 220.0}
 	iou := utils.CalculateIoU(groundTruth, prediction)
 	fmt.Println(iou)
+
+	// 函数底层机制解析
+	n1 := 10
+	// 在调用函数时会为函数开辟单独的栈区空间存储中间变量
+	// 一旦函数执行结束系统就会销毁这个空间
+	function.Test(n1) // 这里Test函数使用完n1后，会结束进程，直接把函数里的n1的空间回收了
+	fmt.Println()
+	fmt.Println("main函数汇中的 n1 =", n1) // 同时呢main栈区里的n1未被销毁，所以这里就输出了原始的n1
+
+	fmt.Println(function.Sum(1222, 22))
+
+	res1, res2 := function.SumSub(13, 7) // 不想要某个结果就直接下划线忽略就行
+	fmt.Printf("和为：%d,差为%d", res1, res2)
+
 }
